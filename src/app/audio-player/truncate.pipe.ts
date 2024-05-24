224996e469f940e843488fda20cff631487a2233
Petko Todorov <p.todorov1015@gmail.com>
2024-05-24 10:18:00 +0300

    small change about playlist songs names

--- src/app/audio-player/truncate.pipe.ts
+++ src/app/audio-player/truncate.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
   transform(value: string, maxLength: number = 40): string {
-    value = value.slice(0, value.length - 4);
     if (value.length <= maxLength) {
       return value;
     } else {
